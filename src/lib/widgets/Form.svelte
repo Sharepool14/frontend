79c7a773c145d6aac20300a8813f270b0ff64111
Theo Johansson <32549413+TheoJ2001@users.noreply.github.com>
2022-04-19 21:07:31 +0200

    added binding for the input element in the Input component and ensuring Input.svelte cannot be a button. Styling for submit button when form is invalid

---
+++ src/lib/widgets/Form.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+	import { createEventDispatcher } from 'svelte';
+
+	const dispatch = createEventDispatcher();
+	
+	const notify = () => {
+		dispatch('submitForm');
+	};
+
+	export let title: string;
+</script>
+
+<h1 class="mb-x1">{title}</h1>
+<form on:submit|preventDefault={notify}>
+	<slot />
+	<button type="submit" class="ml-auto mr-auto">Confirm</button>
+</form>
+
+<style lang="postcss">
+	form {
+		display: flex;
+		flex-direction: column;
+		&:invalid button {
+			cursor: not-allowed;
+		}
+	}
+</style>
---
+++ src/lib/widgets/Input.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+	export let type: string;
+	export let value: string | number | Date;
+	export let required = true;
+	export let inputRef:HTMLInputElement = undefined;
+
+	const setType = (node) => {
+		if (['submit', 'reset', 'button'].includes(type)) {
+			console.error(`'${type}' is an invalid input type, use a button instead`);
+			type = 'text';
+		}
+		node.type = type;
+	};
+</script>
+
+<label class="mb-x1">
+	<slot />
+	<input use:setType bind:value {required} bind:this={inputRef}/>
+</label>
+
+<style lang="postcss">
+	label {
+		display: flex;
+		flex-direction: column;
+		input {
+			border: 1px solid black;
+		}
+	}
+</style>
