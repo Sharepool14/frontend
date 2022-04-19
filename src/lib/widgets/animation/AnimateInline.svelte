a555a4900db0948e3c4891b272bf5cd7f2d96630
Theo Johansson <32549413+TheoJ2001@users.noreply.github.com>
2022-04-19 20:47:51 +0200

    renamed Animate widget to AnimateBlock, made an inline version, moved to animation dir

---
+++ src/lib/widgets/animation/AnimateInline.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import { transitionCleanup } from '../../typescript/ts/global';
+	import { fade } from 'svelte/transition';
+	export let annimationSpeed = 250;
+</script>
+
+<span
+	out:fade={{ duration: annimationSpeed }}
+	in:fade={{ delay: annimationSpeed, duration: annimationSpeed }}
+	on:introend={() => transitionCleanup()}
+>
+	<slot />
+</span>
