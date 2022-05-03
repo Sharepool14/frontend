a4f851b593b6271eab20cadaddb3717f287fc1ed
Theo Johansson <32549413+TheoJ2001@users.noreply.github.com>
2022-05-03 16:50:26 +0200

    moved user authentication to auth.store

--- src/typescript/data/auth.store.ts
+++ src/typescript/data/auth.store.ts
@@ -1,3 +1,31 @@
 import { writable } from 'svelte/store';
 
 export let hasAccess = writable(false);
+
+const url = 'http://localhost:8080';
+
+interface Tokens {
+	access_token: string;
+	refresh_token: string;
+}
+
+export const authenticate = async (login: Authentication) => {
+	try {
+		let response = await fetch(url + '/login', {
+			method: 'POST',
+			headers: {
+				Host: url,
+				'contnent-type': 'application/json',
+			},
+			body: JSON.stringify(login),
+		});
+		let data = (await response.json()) as Tokens;
+		document.cookie = `access_token=${data.access_token};`;
+		document.cookie = `refresh_token=${data.refresh_token};`;
+		hasAccess.set(true);
+		console.log(document.cookie);
+		alert(`${document.cookie}`);
+	} catch (err) {
+		console.error(err);
+	}
+};
