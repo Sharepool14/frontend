bc7b88e3a32bec95ee26f424d7d7eb9009bb5dd2
Theo Johansson <32549413+TheoJ2001@users.noreply.github.com>
2022-06-03 13:21:11 +0200

    Added login guard for feed page

--- src/routes/feed/index.svelte
+++ src/routes/feed/index.svelte
@@ -12,15 +12,14 @@
 <script lang="ts">
 	import { BorrowPost as Post } from '$lib/modules/other';
 	import { Block as Animate } from '$lib/modules/anim';
-	import { CardColored } from '$lib/modules/cards';
 	import { hasAccess } from '$lib/data/auth.store';
-	import { onMount } from 'svelte';
+	import { CardColored } from '$lib/modules/cards';
 
 	export let posts;
 </script>
 
 <Animate>
-	{#if posts}
+	{#if posts && posts.length > 0}
 		<div class="posts minl-auto">
 			{#each posts as post}
 				<Post
@@ -33,6 +32,14 @@
 				/>
 			{/each}
 		</div>
+	{:else}
+		<CardColored --color={'var(--primary)'} --dark={'var(--primary-dark)'}>
+			{#if $hasAccess}
+				<h2>No items are availible at the moment</h2>
+			{:else}
+				<h2>You have to be logged in to see this page</h2>
+			{/if}
+		</CardColored>
 	{/if}
 </Animate>
 
--- src/routes/feed/index.ts
+++ src/routes/feed/index.ts
@@ -5,20 +5,24 @@ export const get: RequestHandler = async ({ request }) => {
 	const { body } = await getHandler('/user/community', request);
 	const posts = [];
 
-	body.forEach((community) => {
-		community.loan_posts.forEach((post) => {
-			const startTime = new Date(post.start_date).getTime();
-			const endTime = new Date(post.return_date).getTime();
-			if (Date.now() - startTime >= endTime - startTime) {
-				post.visible = false;
-			}
-			if (post.visible) {
-				if (!posts.find((feedPost) => feedPost.item.id == post.item.id)) {
-					posts.push(post);
+	try {
+		body.forEach((community) => {
+			community.loan_posts.forEach((post) => {
+				const startTime = new Date(post.start_date).getTime();
+				const endTime = new Date(post.return_date).getTime();
+				if (Date.now() - startTime >= endTime - startTime) {
+					post.visible = false;
 				}
-			}
+				if (post.visible) {
+					if (!posts.find((feedPost) => feedPost.item.id == post.item.id)) {
+						posts.push(post);
+					}
+				}
+			});
 		});
-	});
+	} catch (err) {
+		console.warn(err);
+	}
 
 	return {
 		body: {
